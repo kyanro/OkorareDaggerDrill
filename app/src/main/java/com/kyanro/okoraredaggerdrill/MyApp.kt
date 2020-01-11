6fd9a2da2182027c551a8a8b968ca929af94f6c7
kyanro <kyanro@users.noreply.github.com>
2020-01-12 01:05:17 +0900

    起動時にコンポーネントを作成してアプリケーションクラスで保持しておく

--- app/src/main/java/com/kyanro/okoraredaggerdrill/MyApp.kt
+++ app/src/main/java/com/kyanro/okoraredaggerdrill/MyApp.kt
@@ -1,9 +1,13 @@
 package com.kyanro.okoraredaggerdrill
 
 import android.app.Application
+import com.kyanro.okoraredaggerdrill.dagger.AppComponent
+import com.kyanro.okoraredaggerdrill.dagger.DaggerAppComponent
 
 class MyApp : Application() {
+    lateinit var appComponent: AppComponent
     override fun onCreate() {
         super.onCreate()
+        appComponent = DaggerAppComponent.create()
     }
 }
