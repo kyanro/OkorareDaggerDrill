f3dd24fc2de43e1880a1f7782d0499a9750b07f1
kyanro <kyanro@users.noreply.github.com>
2020-01-12 01:52:18 +0900

    アプリ起動中は同じインスタンスを利用するように、スコープを作成して付加する
    # Conflicts:
    #	app/src/main/java/com/kyanro/okoraredaggerdrill/dagger/DaggerSettings.kt

--- app/src/main/java/com/kyanro/okoraredaggerdrill/dagger/DaggerSettings.kt
+++ app/src/main/java/com/kyanro/okoraredaggerdrill/dagger/DaggerSettings.kt
@@ -4,8 +4,10 @@ import dagger.Component
 import dagger.Module
 import dagger.Provides
 import javax.inject.Named
+import javax.inject.Scope
 import kotlin.random.Random
 
+@AppScope
 @Component(modules = [LuckyNumberModule::class])
 interface AppComponent {
     @Named("lucky number text")
@@ -14,8 +16,12 @@ interface AppComponent {
 
 @Module
 class LuckyNumberModule {
+    @AppScope
     @Provides
     @Named("lucky number text")
     fun provideLuckyNumberText() =
         "今日のラッキーナンバー: ${Random.nextInt(1, 1000)}"
 }
+
+@Scope
+annotation class AppScope
