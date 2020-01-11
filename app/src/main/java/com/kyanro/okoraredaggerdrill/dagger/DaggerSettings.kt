07de2d44670c308bd3ea7c26f05fbe7cdff77781
kyanro <kyanro@users.noreply.github.com>
2020-01-12 02:17:58 +0900

    今日の運勢を表示するためのモジュールを追加する

--- app/src/main/java/com/kyanro/okoraredaggerdrill/dagger/DaggerSettings.kt
+++ app/src/main/java/com/kyanro/okoraredaggerdrill/dagger/DaggerSettings.kt
@@ -7,10 +7,13 @@ import javax.inject.Named
 import javax.inject.Scope
 
 @AppScope
-@Component(modules = [LuckyNumberModule::class])
+@Component(modules = [LuckyNumberModule::class, FortuneModule::class])
 interface AppComponent {
     @Named("lucky number text")
     fun getLuckyNumberText(): String
+
+    @Named("fortune text")
+    fun getFortuneText(): String
 }
 
 @Module
@@ -20,6 +23,22 @@ class LuckyNumberModule(private val luckyNumber: Int) {
     @Named("lucky number text")
     fun provideLuckyNumberText() =
         "今日のラッキーナンバー: $luckyNumber"
+}
+
+@Module
+class FortuneModule(private val luckyNumber: Int) {
+    @AppScope
+    @Provides
+    @Named("fortune text")
+    fun provideFortuneText(): String {
+        val fortune = when (luckyNumber) {
+            in 0..99 -> "アゲアゲ！！！"
+            in 100..499 -> "アゲ！！"
+            in 500..899 -> "サゲ！"
+            else -> "サゲサゲ"
+        }
+        return "今日の運勢: $fortune"
+    }
 }
 
 @Scope
